4d38cc028dd8f2aa87c97c13857a5dd75895a257
ctl3d32 <93521015+ctl3d32@users.noreply.github.com>
2022-09-10 17:08:35 -0300

    Update teste2.lua

--- teste2.lua
+++ teste2.lua
@@ -2,6 +2,35 @@ while #game:GetService("Players"):GetPlayers() == 0 and (not game:GetService("Pl
 	wait()
 end
 
+
+game:GetService("Players").LocalPlayer.CharacterAdded:wait()
+
+local function TryTeleportToShroomcalipse()
+	if game.PlaceId == 3852057184 then
+		print('teleporting from Crossroads to Shroomcalipse...')
+		local args = {
+			[1] = "dungeonTeleport",
+			[2] = "mushtropolis",
+			[3] = {
+				["difficulty"] = 4
+			}
+		}
+		game:GetService("ReplicatedStorage").playerRequest:InvokeServer(unpack(args))
+	elseif 	game.PlaceId == 3273679677 then
+		print('You are at Shroomcalipse')
+	end
+end
+
+game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+    if State == Enum.TeleportState.Started then
+        syn.queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/ctl3d32/Teste/main/teste2.lua',true))()")
+    end
+end)
+
+TryTeleportToShroomcalipse()while #game:GetService("Players"):GetPlayers() == 0 and (not game:GetService("Players").LocalPlayer) do
+	wait()
+end
+
 if  not game:GetService("Players").LocalPlayer.Character then
 	game:GetService("Players").LocalPlayer.CharacterAdded:wait()
 end
