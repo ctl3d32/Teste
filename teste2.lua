49ccb36a62397d647eba8eb9d481efd0f97c3faa
ctl3d32 <93521015+ctl3d32@users.noreply.github.com>
2022-09-10 16:20:17 -0300

    Add files via upload

---
+++ teste2.lua
@@ -0,0 +1,23 @@
+local function TryTeleportToShroomcalipse()
+	if game.PlaceId == 3852057184 then
+		print('teleporting from Crossroads to Shroomcalipse...')
+		local args = {
+			[1] = "dungeonTeleport",
+			[2] = "mushtropolis",
+			[3] = {
+				["difficulty"] = 4
+			}
+		}
+		game:GetService("ReplicatedStorage").playerRequest:InvokeServer(unpack(args))
+	elseif 	game.PlaceId == 3273679677 then
+		print('You are at Shroomcalipse')
+	end
+end
+
+game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
+    if State == Enum.TeleportState.Started then
+        syn.queue_on_teleport("Wait(15) loadstring(game:HttpGet('https://github.com/ctl3d32/Teste/blob/main/teste2.lua',true))()")
+    end
+end)
+
+TryTeleportToShroomcalipse()
